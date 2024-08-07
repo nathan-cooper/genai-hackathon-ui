252274dc57f47cfb191ee4028590447ca0043b88
Cooper <nathan.cooper834@gmail.com>
2024-08-07 08:36:49 -0500

    fix lint issue

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -117,7 +117,7 @@ const ProductDetailPage = () => {
                         <ColContainer item container direction={"column"} md={2.4} xs={12}
                             className="pr-12 pl-12"
                         >
-                            <Button variant="contained">Apply Now</Button>
+                            <Button variant="contained" onClick={handleApplyNow}>Apply Now</Button>
                             <CreditTermsLink />
                         </ColContainer>
                     </RowContainer>
